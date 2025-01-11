2a4bae49b9118165d32bad6a22daa5fddbe8e7ff
johan-stph <111685646+johan-stph@users.noreply.github.com>
2025-01-11 22:03:03 +0100

    added spring security

---
+++ backend/src/main/java/com/icyderdritte/backend/health/HealthController.java
@@ -0,0 +1,18 @@
+package com.icyderdritte.backend.health;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+
+    @GetMapping
+    public ResponseEntity<String> helloWorldUnauthenticated() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
